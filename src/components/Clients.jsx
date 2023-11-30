7596a622347de99853fcc54405e2b6f993ba36db
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-11-30 18:48:25 +0100

    Clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,22 @@
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+import { clients } from "../constants"
+
+const Clients = () => (
+  <section className='flexCenter} my-4'>
+    <div className='flexCenter flex-wrap w-full'>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className='flex-1 flexCenter sm:min-w-[192px] min-w-[120px] m-5'
+        >
+          <img
+            src={client.logo}
+            alt='client_logo'
+            className='sm:w-[192px] w-[100px] object-contain hover:contrast-50'
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+)
 
 export default Clients
