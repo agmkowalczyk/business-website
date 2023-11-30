563c085fa051cadd0a78d4946393eac658aeadc8
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-11-30 18:12:25 +0100

    Business

--- src/components/index.js
+++ src/components/index.js
@@ -9,6 +9,7 @@ import Footer from './Footer'
 import Testimonials from './Testimonials'
 import Hero from './Hero'
 import GetStarted from './GetStarted'
+import Button from './Button'
 
 export {
   Navbar,
@@ -22,4 +23,5 @@ export {
   Testimonials,
   Hero,
   GetStarted,
+  Button,
 }
